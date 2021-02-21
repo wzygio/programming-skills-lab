29b51337d673d0b57fefdf4c15294be5ce9386be
wzygio <wzygio@sina.com>
2021-02-21 20:34:28 +0800

    clear

--- shell/count.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash 
-COUNT=1
-while [  $COUNT -lt 1000000000 ]; do
-    sleep 1
-    echo $COUNT
-    let COUNT=COUNT+1 
-done
--- shell/variables.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-EXAMPLE_EXE="wordcount.py"
-EXAMPLE_DIR="make/"
-EXAMPLE_PATH=tmp:$PATH
-echo $EXAMPLE_EXE
-echo $EXAMPLE_DIR
-echo $EXAMPLE_PATH
